2392049a4128bb3b4d4ee1495fc86973490f92ed
Leducphat1234 <96918446+Leducphat1234@users.noreply.github.com>
2022-08-03 13:39:26 +0700

    Update server.py

--- server.py
+++ server.py
@@ -1,22 +1,47 @@
-def main():
-    import socket
-    HOST = socket.gethostbyname(socket.gethostname())
+import socket
+import datetime
+import threading
+HOST = socket.gethostbyname(socket.gethostname())
+FORMAT = "utf-8"
+log = open("log.txt", "a")
+def keyboard_receiver():
     PORT = 4000
-    FORMAT = "utf-8"
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((HOST, PORT))
         s.listen()
         while True:
             conn, addr = s.accept()
-            HOSTNAMECLIENT = conn.recv(64).decode()
-            print("$Connection accepted$")
-            print(HOSTNAMECLIENT)
+            HOSTNAMECLIENT = conn.recv(64).decode(FORMAT)
+            print(HOSTNAMECLIENT, addr)
+            log.write(HOSTNAMECLIENT + " " + str(addr))
             while True:
+                timenow = datetime.datetime.now()
+                now = timenow.strftime("%H:%M:%S")
                 try:
                     data = conn.recv(64).decode(FORMAT)
-                    print(data)
+                    print(now + " " + data)
+                    
+                    log.write(now + " " + data + "\n")
+                except ConnectionResetError:
+                    break
+def mouse_receiver():
+    PORT = 5000
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+        while True:
+            conn, addr = s.accept()
+            while True:
+                timenow = datetime.datetime.now()
+                now = timenow.strftime("%H:%M:%S")
+                try:
+                    data = conn.recv(64).decode(FORMAT)
+                    print(now + " " + data)
+                    
+                    log.write(now + " " + data + "\n")
                 except ConnectionResetError:
                     break
 
 if __name__ == "__main__":
-    main()
+    threading.Thread(target=keyboard_receiver).start()
+    mouse_receiver()
